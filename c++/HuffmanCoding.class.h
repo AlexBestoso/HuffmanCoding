636030cbab79443819dcd894564b272be50b408d
Morning Star <alexbestoso@gmail.com>
2025-11-28 18:31:25 -0500

    sort frequency table

--- c++/HuffmanCoding.class.h
+++ c++/HuffmanCoding.class.h
@@ -1,3 +1,4 @@
+#define HUFFMAN_DEBUGGING 1
 class HuffmanCoding{
 	private:
 		char *treeLetters;
@@ -29,7 +30,7 @@ class HuffmanCoding{
 				this->destroyTreeLetters();
 				this->treeLetters = new char[size];
 				this->treeLetters_s = size;
-				for(int i=0; i<this->treeLetters_s && i<oldSize){
+				for(int i=0; i<this->treeLetters_s && i<oldSize; i++){
 					this->treeLetters[i] = transfer[i];
 				}
 				delete[] transfer;
@@ -55,7 +56,7 @@ class HuffmanCoding{
                                 this->destroyFrequencies();
                                 this->frequencies = new int[size];
                                 this->frequencies_s = size;
-                                for(int i=0; i<this->frequencies_s && i<oldSize){
+                                for(int i=0; i<this->frequencies_s && i<oldSize; i++){
                                         this->frequencies[i] = transfer[i];
                                 }
                                 delete[] transfer;
@@ -82,7 +83,7 @@ class HuffmanCoding{
 				char testChar = data[i];
 				bool matched = false;
 				for(int j=0; j<this->treeLetters_s; j++){
-					if(testChar == this->treeLetters[i]){
+					if(testChar == this->treeLetters[j]){
 						matched = true;
 						break;
 					}
@@ -127,10 +128,84 @@ class HuffmanCoding{
 			return true;
 		}
 
-		bool encode(void){
-			// sort frequency and letter tables so that the lower frequencies are first.
-			// node count is frequencies_s - 1
+		void sortFreqencies(void){
+			if(this->treeLetters_s != this->frequencies_s)
+				return;
+			if(this->treeLetters == NULL || this->frequencies == NULL)
+				return;
+
+			size_t sortList_s = this->frequencies_s;
+			int *sortList = new int[sortList_s];
+			int *indexList = new int[sortList_s];
+			int indexI = 0;
+			for(int i=0; i<sortList_s; i++){
+				sortList[i] = this->frequencies[i];
+			}
 			
+			while(sortList_s > 0){
+				// Identify smallest value.
+				int biggest=sortList[0];
+				for(int i=0; i<sortList_s; i++){
+					if(sortList[i] > biggest)
+						biggest = sortList[i];
+				}
+				
+				// Gather indecies of smallest values
+				int matchCount = 0;
+				for(int i=0; i<this->frequencies_s && indexI<this->frequencies_s; i++){
+					if(this->frequencies[i] == biggest){
+						indexList[indexI] = i;
+						indexI++;
+						matchCount++;
+					}
+				}
+
+				// strip sort list of the smallest value.
+				size_t transfer_s = sortList_s-matchCount;
+				int *transfer = new int[transfer_s];
+				for(int i=0, t=0; i<sortList_s && t<sortList_s-matchCount; i++){
+					if(sortList[i] != biggest){
+						transfer[t] = sortList[i];
+						t++;
+					}
+				}
+				delete[] sortList;
+				sortList = NULL;
+				sortList_s = transfer_s;
+				if(sortList_s > 0){
+					sortList = new int[sortList_s];
+					for(int i=0; i<sortList_s; i++){
+						sortList[i] = transfer[i];
+					}
+				}
+				delete[] transfer;
+			}
+			
+			// Use index list to re-organize frequency and tree letter arrays.
+			if(sortList != NULL)
+				delete[] sortList;
+			sortList = new int[this->frequencies_s];
+			char *sortList2 = new char[this->frequencies_s];
+			for(int i=0; i<this->frequencies_s; i++){
+				int targetIndex = indexList[i];
+				if(targetIndex >= this->frequencies_s)
+					break;
+				sortList[i] = this->frequencies[targetIndex];
+				sortList2[i] = this->treeLetters[targetIndex];
+			}
+			
+			for(int i=0; i<this->frequencies_s; i++){
+				this->frequencies[i] = sortList[i];
+				this->treeLetters[i] = sortList2[i];
+			}
+
+			delete[] sortList2;
+			delete[] sortList;
+			delete[] indexList;
+		}
+
+		bool encode(char *data, size_t dataSize){
+				
 			return true;
 		}
 
@@ -175,7 +250,7 @@ class HuffmanCoding{
 
 		bool compress(char *data, size_t dataSize){
 			this->clearError();
-			this->destroyTree();
+			this->destroyTreeLetters();
 			this->destroyFrequencies();
 			this->destroyOut();
 			if(data == NULL){
@@ -197,6 +272,25 @@ class HuffmanCoding{
 				return false;
 			}
 
+			#if HUFFMAN_DEBUGGING == 1
+			printf("[DBG] Frequencies : \n\t");
+			for(int i=0; i<this->frequencies_s; i++)
+				printf("'%c'(%d) ", this->treeLetters[i], this->frequencies[i]);
+			printf("\n");
+			#endif
+
+			this->sortFreqencies();
+			#if HUFFMAN_DEBUGGING == 1
+			printf("[DBG] Sorted Frequencies : \n\t");
+			for(int i=0; i<this->frequencies_s; i++)
+				printf("'%c'(%d) ", this->treeLetters[i], this->frequencies[i]);
+			printf("\n");
+			#endif
+
+			if(!this->encode(data, dataSize)){
+				this->setError(0x004, "compress(char *data, size_t dataSize) - Failed to encode data.");
+				return false;
+			}
 			
 
 			return true;
@@ -204,7 +298,7 @@ class HuffmanCoding{
 
 		bool decompress(char *data, size_t dataSize){
 			this->clearError();
-			this->destroyTree();
+			this->destroyTreeLetters();
 			this->destroyFrequencies();
 			this->destroyOut();
 			return true;
--- c++/main.cc
+++ c++/main.cc
@@ -6,8 +6,16 @@
 
 int main(void){
 	printf("Huffman Coding Tests\n");
+	HuffmanCoding hc;
 
 	std::string msg = "What I always found interesting was the fact that I can just write for days and days, seemingly never running out of things to say.";
-	printf("Encoding the message (%ld) : \n\t%s\n", msg.length(), msg.c_str());
+	printf("Compressing the message (%ld) : \n\t%s\n", msg.length(), msg.c_str());
+
+	if(!hc.compress((char *)msg.c_str(), msg.length())){
+		printf("Compression failed : %s\n", hc.getErrorMessage().c_str());
+		exit(EXIT_FAILURE);
+	}
+	printf("NOT YET FINISHED\n");
+	
 	exit(EXIT_SUCCESS);
 }
